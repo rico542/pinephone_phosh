518f06b6a9342ab7893264d9eaa97db30c4c79a6
rico <rico@alwaysdata.net>
2020-02-25 06:36:45 +0400

    Merge A-WAI updates
    fix desktop background
    enable sound
    enable modem
    enable graphics acceleration
    send/receive text messages
    display splash screen during linux boot
    
    modified file :         build.sh
    modified file :         files/u-boot-sunxi-with-spl.bin
    modified file :         image.yaml
    new file : overlays/rootfs/etc/kernel/postinst.d/zz-u-boot
    new file : overlays/rootfs/usr/bin/pinephone-modem.sh
    new file : overlays/rootfs/usr/lib/systemd/system/ModemManager.service
    new file : overlays/rootfs/usr/lib/systemd/system/pinephone-modem.service

--- build.sh
+++ build.sh
@@ -2,5 +2,17 @@
 
 export PATH=/sbin:/usr/sbin:$PATH
 
-debos --scratchsize=8G rootfs.yaml &&
-debos --scratchsize=8G image.yaml
+if [ "$http_proxy" ]
+then
+  ARGS="-e http_proxy:\"$http_proxy\""
+fi
+
+if [ "$ftp_proxy" ]
+then
+  ARGS="$ARGS -e ftp_proxy:\"$ftp_proxy\""
+fi
+
+ARGS="$ARGS --scratchsize=8G"
+
+debos $ARGS rootfs.yaml &&
+debos $ARGS image.yaml
